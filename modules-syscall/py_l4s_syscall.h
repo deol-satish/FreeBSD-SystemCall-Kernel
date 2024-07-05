a16b71a25ebf3bc83e9db1b62e51e3f72628776e
deol-satish <164171952+deol-satish@users.noreply.github.com>
2024-07-05 20:00:13 +1000

    Developing Python wrapper for system calls

---
+++ modules-syscall/py_l4s_syscall.h
@@ -0,0 +1,30 @@
+#ifndef DRL_H
+#define DRL_H
+
+#include <stdint.h>
+
+struct data {
+    uint32_t flows_cnt;
+    uint32_t limit;
+    uint32_t quantum;
+    uint64_t tot_pkts;
+    uint64_t tot_bytes;
+    uint32_t length;
+    uint32_t len_bytes;
+    uint32_t drops;
+    uint16_t max_ecnth;
+    uint16_t alpha;
+    uint16_t beta;
+    uint32_t burst_allowance;
+    uint32_t drop_prob;
+    uint32_t current_qdelay;
+    uint32_t qdelay_old;
+    uint64_t accu_prob;
+    uint32_t avg_dq_time;
+    uint32_t dq_count;
+};
+
+int drl_update_prob(int prob);
+int drl_get_buffer(void *data, int *size);
+
+#endif // DRL_H
